daa09ddc86c575932b7b6595dca86102aae0e002
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-29 17:21:54 +0530

    impliment a pagination features on post

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -3,6 +3,7 @@ import { Post, PostInterface } from "../models/post.model.js";
 import { HydratedDocument } from "mongoose";
 import { AuthRequest } from "../middlewares/auth.middleware.js";
 
+
 export const createPost = async (req: AuthRequest, res: Response): Promise < void > => {
         try {
 
@@ -55,3 +56,75 @@ export const createPost = async (req: AuthRequest, res: Response): Promise < voi
             return;
         }
 } 
+
+export const gettingPost = async (req: AuthRequest, res: Response): Promise < void > => {
+    try {        
+        const posts = await Post.find().populate("writer", "name");
+        if(!posts) {
+            res.status(404).json({
+                success: false,
+                message: "post not found."
+            })
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            posts: posts
+        })
+
+    } catch (error) {
+        console.log(`error while getting all posts: ${error}`);
+        
+        res.status(500).json({
+            success: false,
+            message: `server error something went wrong : ${error}`
+        })
+
+        return;
+    }
+}
+
+
+export const postsInPagination = async (req: AuthRequest, res: Response): Promise < void > => {
+    try {
+        const { page, limit } = req.params as {
+            page: string,
+            limit: string
+        };
+
+        if( parseInt(page) <= 0 || parseInt(limit) <= 0 ) {
+            res.status(400).json({
+                success: false,
+                message: "page or limit must be greater than 0"
+            })
+            return;
+        }
+
+        const options = {
+            page: parseInt(page),
+            limit: parseInt(limit)
+        }
+
+        const posts = Post.paginate({}, options);
+        if(!posts) {
+            res.status(404).json({
+                success: false,
+                message: "post not found."
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            posts: posts 
+        })
+
+
+    } catch (error) {
+        console.log(`error while getting posts in pagination formate: ${error}`);
+        res.status(500).json({
+            success: false,
+            message: `server error something went wrong : ${error}`
+        })
+    }
+}
