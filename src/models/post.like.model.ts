539c0fc907e83ac41433bb389044f97bcd5233ac
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-30 18:19:59 +0530

    post like  model

---
+++ src/models/post.like.model.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+export interface PostLikeType {
+    userId: mongoose.Types.ObjectId,
+    postId: mongoose.Types.ObjectId,
+    like?: number
+}
+
+const postLikeSchema = new mongoose.Schema < PostLikeType >({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    postId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+        required: true
+    },
+
+    like: {
+        type: Number,
+        default: 0
+    }
+}, {timestamps: true});
+
+
+export const Like = mongoose.model < PostLikeType > ("Like", postLikeSchema);
