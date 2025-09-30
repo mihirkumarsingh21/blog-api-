6d5eb295df5394803affd7038adbe9817d425db5
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-09-30 18:18:51 +0530

    add some fatures in post

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,130 +1,250 @@
 import type { Response } from "express";
 import { Post, PostInterface } from "../models/post.model.js";
-import { HydratedDocument } from "mongoose";
+import { HydratedDocument, isValidObjectId } from "mongoose";
 import { AuthRequest } from "../middlewares/auth.middleware.js";
 
+export const createPost = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { title, description, like, image } = req.body as {
+      title: string;
+      description: string;
+      like?: number;
+      image?: string;
+    };
 
-export const createPost = async (req: AuthRequest, res: Response): Promise < void > => {
-        try {
-
-            const { title, description, like, image } = req.body as {
-                title: string,
-                description: string,
-                like?: number,
-                image?: string
-            };
-
-            if(!title || !description) {
-                res.status(400).json({
-                    success: false,
-                    message: "Title and Description fields are required."
-                })
-                return;
-            }
-
-            type PostDocumentType = HydratedDocument < PostInterface >
-
-            const createdPost: PostDocumentType = await Post.create({
-                writer: req.userId,
-                title,
-                description,
-                like,
-                image
-            });
-
-            if(!createdPost) {
-                res.status(400).json({
-                    success: false,
-                    message: "Failed to create post."
-                })
-                return;
-            }
-
-            res.status(200).json({
-                success: true,
-                posts: createdPost
-            })
-
-        } catch (error) {
-            console.log(`error while creating post: ${error}`);
-            
-            res.status(500).json({
-                success: false,
-                message: `server error something went wrong: ${error}`
-            })
-
-            return;
-        }
-} 
-
-export const gettingPost = async (req: AuthRequest, res: Response): Promise < void > => {
-    try {        
-        const posts = await Post.find().populate("writer", "name");
-        if(!posts) {
-            res.status(404).json({
-                success: false,
-                message: "post not found."
-            })
-            return;
-        }
-
-        res.status(200).json({
-            success: true,
-            posts: posts
-        })
-
-    } catch (error) {
-        console.log(`error while getting all posts: ${error}`);
-        
-        res.status(500).json({
-            success: false,
-            message: `server error something went wrong : ${error}`
-        })
-
-        return;
+    if (!title || !description) {
+      res.status(400).json({
+        success: false,
+        message: "Title and Description fields are required.",
+      });
+      return;
     }
-}
 
+    type PostDocumentType = HydratedDocument<PostInterface>;
 
-export const postsInPagination = async (req: AuthRequest, res: Response): Promise < void > => {
-    try {
-        const { page, limit } = req.params as {
-            page: string,
-            limit: string
-        };
+    const createdPost: PostDocumentType = await Post.create({
+      writer: req.userId,
+      title,
+      description,
+      like,
+      image,
+    });
 
-        if( parseInt(page) <= 0 || parseInt(limit) <= 0 ) {
-            res.status(400).json({
-                success: false,
-                message: "page or limit must be greater than 0"
-            })
-            return;
-        }
-
-        const options = {
-            page: parseInt(page),
-            limit: parseInt(limit)
-        }
-
-        const posts = await Post.paginate({}, options);
-        if(!posts) {
-            res.status(404).json({
-                success: false,
-                message: "post not found."
-            })
-        }
-
-        res.status(200).json({
-            success: true,
-            posts: posts 
-        })
-
-
-    } catch (error) {
-        console.log(`error while getting posts in pagination formate: ${error}`);
-        res.status(500).json({
-            success: false,
-            message: `server error something went wrong : ${error}`
-        })
+    if (!createdPost) {
+      res.status(400).json({
+        success: false,
+        message: "Failed to create post.",
+      });
+      return;
     }
-}
+
+    res.status(200).json({
+      success: true,
+      posts: createdPost,
+    });
+  } catch (error) {
+    console.log(`error while creating post: ${error}`);
+
+    res.status(500).json({
+      success: false,
+      message: `server error something went wrong: ${error}`,
+    });
+
+    return;
+  }
+};
+
+export const gettingPost = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const posts = await Post.find().populate("writer", "name");
+    if (!posts) {
+      res.status(404).json({
+        success: false,
+        message: "post not found.",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      posts: posts,
+    });
+  } catch (error) {
+    console.log(`error while getting all posts: ${error}`);
+
+    res.status(500).json({
+      success: false,
+      message: `server error something went wrong : ${error}`,
+    });
+
+    return;
+  }
+};
+
+export const postsInPagination = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    let { page, limit, sort } = req.params as {
+      page: string;
+      limit: string;
+      sort?: string;
+    };
+
+    let sortedValue = Number(sort);
+    sortedValue = -1;
+
+    if (parseInt(page) <= 0 || parseInt(limit) <= 0) {
+      res.status(400).json({
+        success: false,
+        message: "page or limit must be greater than 0",
+      });
+      return;
+    }
+
+    const options = {
+      page: parseInt(page),
+      limit: parseInt(limit),
+      sort: {
+        createdAt: sortedValue,
+      },
+    };
+
+    const posts = await Post.paginate({}, options);
+    if (!posts) {
+      res.status(404).json({
+        success: false,
+        message: "post not found.",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      posts: posts,
+    });
+  } catch (error) {
+    console.log(`error while getting posts in pagination formate: ${error}`);
+    res.status(500).json({
+      success: false,
+      message: `server error something went wrong : ${error}`,
+    });
+  }
+};
+
+export const updatePost = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { postId } = req.params as {
+      postId: string;
+    };
+
+    const { title, description } = req.body as {
+      title: string;
+      description: string;
+    };
+
+    if (!postId) {
+      res.status(400).json({
+        success: false,
+        message: "post id are not present in your url.",
+      });
+      return;
+    }
+
+    if (!isValidObjectId(postId)) {
+      res.status(400).json({
+        success: false,
+        message: "Invalid post id.",
+      });
+
+      return;
+    }
+
+    const updatedPost = await Post.findByIdAndUpdate(
+      postId,
+      { title, description },
+      { new: true }
+    );
+
+    if (!updatedPost) {
+      res.status(400).json({
+        success: false,
+        message: "Failed to update post.",
+      });
+    }
+
+    res.status(201).json({
+      success: true,
+      message: "your post updated successfully.",
+      updatedPost: updatedPost,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `server error something went wrong: ${error}`,
+    });
+
+    console.log(`error while updating post : ${error}`);
+  }
+};
+
+export const deletePost = async (
+  req: AuthRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const { postId } = req.params as {
+      postId: string;
+    };
+
+    if (!postId) {
+      res.status(400).json({
+        success: false,
+        message: "post id are not present in your url.",
+      });
+      return;
+    }
+
+    if (!isValidObjectId(postId)) {
+      res.status(400).json({
+        success: false,
+        message: "Invalid post id.",
+      });
+      return;
+    }
+
+    const deletedPost = await Post.findByIdAndDelete(postId);
+    if (!deletedPost) {
+      res.status(400).json({
+        success: false,
+        message: "Failed to delete your post or this is already deleted.",
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "your post deleted successfully.",
+    });
+  } catch (error) {
+    console.log(`error while deleting post : ${error}`);
+
+    res.status(500).json({
+      success: false,
+      message: `server error something went wrong : ${error}`,
+    });
+
+    return;
+  }
+};
+
+
