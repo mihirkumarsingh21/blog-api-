a11a137aaadd55c7ebe2b73a194b8f31b8959ccf
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-02 01:40:07 +0530

    add create, upate,  delete route in the post route

--- src/routes/posts.routes.ts
+++ src/routes/posts.routes.ts
@@ -1,14 +1,15 @@
 import express from "express";
 import { authProtect } from "../middlewares/auth.middleware.js";
 import { createPost, deletePost, gettingPost, postsInPagination, updatePost } from "../controllers/post.controller.js";
+import { author } from "../middlewares/author.middleware.js";
 
 const route = express.Router();
 
 route.post("/create", authProtect, createPost);
 route.get("/get-post", authProtect, gettingPost);
 route.get("/get-post/:page/:limit", authProtect, postsInPagination);
-route.put("/update-post/:postId", authProtect, updatePost);
-route.delete("/delete/:postId", authProtect, deletePost);
+route.put("/update-post/:postId", authProtect, author, updatePost);
+route.delete("/delete/:postId", authProtect, author, deletePost);
 
 
 
