12f82309ffb7bc878a8a7ece98ccdb1865f59ce5
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-02 01:38:47 +0530

    little bit change in post like route file

--- src/routes/post.like.route.ts
+++ src/routes/post.like.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { postLike } from "../controllers/post.like.controller.js";
+import { authProtect } from "../middlewares/auth.middleware.js";
 
 const route = express.Router();
 
-route.post("/like-post/:postId", postLike);
+route.post("/like-post/:postId", authProtect, postLike);
 
 
 
