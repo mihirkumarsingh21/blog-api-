eb320860b04808d689d5d68cc4fc664299f3e503
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-06 20:19:33 +0530

    Add user profile update and follow functionality routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,6 +1,6 @@
 import express from "express";
-import { registerUser, loginUser, logoutUser} from "../controllers/user.controller.js";
-// import { authProtect } from "../middlewares/auth.middleware.js";
+import { registerUser, loginUser, logoutUser, updatingAuthUserProfile, gettingAuthUserProfile, followTheUser} from "../controllers/user.controller.js";
+import { authProtect } from "../middlewares/auth.middleware.js";
 
 
 const route = express.Router();
@@ -8,5 +8,8 @@ const route = express.Router();
 route.post("/register", registerUser);
 route.post("/login", loginUser);
 route.get("/logout", logoutUser);
+route.put("/profile/update", authProtect, updatingAuthUserProfile);
+route.get("/profile/:userId", gettingAuthUserProfile);
+route.patch("/follow/:userId", authProtect, followTheUser);
 
 export default route
