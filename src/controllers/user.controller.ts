5a50627eba8178ce89c588ad60f7b45d48748b9b
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-06 20:18:14 +0530

    Add user profile update, retrieval, and follow/unfollow functionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,8 @@
 import type { Request, Response } from "express";
 import { User, type UserInterface } from "../models/user.model.js";
+import { AuthRequest } from "../middlewares/auth.middleware.js";
 import bcrypt from "bcryptjs";
-import mongoose from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 // import { AuthRequest } from "../middlewares/auth.middleware.js";
 import { generateJsonWebTokenAndSetCookie } from "../utils/generateJwtTokenAndSetCookie.js";
 
@@ -142,6 +143,157 @@ export const logoutUser = ( req: Request, res: Response ): void => {
         })
         console.log(`error while log out user: ${error}`);
         
+    }
+}
+
+
+export const updatingAuthUserProfile = async (req: AuthRequest, res: Response): Promise < void > => {
+    try {
+        const { profilePic } = req.body as {
+            profilePic: string
+        }
+
+        if(!profilePic) {
+            res.status(400).json({
+                success: false,
+                message: "Profile pic not provided."
+            })
+            return;
+        }
+
+        const user = await User.findByIdAndUpdate(req.userId, { $set: { profilePic } }, {new: true});
+        if(!user) {
+            res.status(400).json({
+                success: false,
+                message: "Failed to update user profile."
+            })
+            return;
+        }
+
+        res.status(201).json({
+            success: true,
+            message: "your profile updated successfully."
+        })
+
+
+    } catch (error) {
+        console.log(`error while updating auth user profile: ${error}`);
+        
+        res.status(500).json({
+            success: false,
+            message: `server error something went wrong : ${error}`
+        })
+
+        return;
+    }
+}
+
+
+export const gettingAuthUserProfile = async (req: AuthRequest, res: Response): Promise < void > => {
+    try {
+
+        const {userId} = req.params as {
+            userId: string
+        }
+
+        if(!userId) {
+            res.status(400).json({
+                success: false,
+                message: "user id are not present in your url."
+            })
+            return;
+        }
+
+        if(!isValidObjectId(userId)) {
+            res.status(400).json({
+                success: false,
+                message: "Invalid user id."
+            })
+            return;
+        }
+
+        const user = await User.findById(userId).select("-email -passwordHash");
+        if(!user) {
+            res.status(404).json({
+                success: false,
+                message: "user is not present with this id."
+            })
+            return;
+        }
+
+        res.status(200).json({
+            success: true,
+            userProfiles: user
+        })
+
+    } catch (error) {
+        console.log(`error while getting auth user profile: ${error}`);
+        
+        res.status(500).json({
+            success: false,
+            message: `server error something went wrong :${error}`
+        })
+        return;
+    }
+}
+
+export const followTheUser = async (req: AuthRequest, res: Response): Promise < void > => {
+    try {
+        const {userId} = req.params;
+        if(!userId) {
+            res.status(400).json({
+                success: false,
+                message: "user id are not present in your url."
+            })
+            return;
+        }
+
+        if(!isValidObjectId(userId)) {
+            res.status(400).json({
+                success: false,
+                message: "Invalid user id."
+            })
+            return;
+        }
+
+        const user = await User.findOne({
+            $or:[
+            {
+                followers: userId
+            },
+            {
+                following: userId
+            }
+        ]
+    });
+        if(!user) {
+            await User.findByIdAndUpdate(userId, {$addToSet: { followers: req.userId }});
+            await User.findByIdAndUpdate(req.userId, {$addToSet: { following: userId }});
+            res.status(200).json({
+                success: true,
+                message: "user follow."
+            })
+            return;
+
+        } else {
+            await User.findByIdAndUpdate(userId, {$pull: {followers: req.userId}});
+            await User.findByIdAndUpdate(req.userId, {$pull: {following: userId}});
+            res.status(200).json({
+                success: true,
+                message: "user unfollow."
+            })
+            return;
+        }
+
+    } catch (error) {
+        console.log(`error while follow the user :${error}`);
+        
+        res.status(500).json({
+            success: false,
+            message: `server error something went wrong : ${error}`
+        })
+
+        return;
     }
 }
 
