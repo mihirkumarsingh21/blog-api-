291bb84abef65c262848a0f2ef7d1747d71a4887
mihirkumarsingh21 <singhmihirkumar783@gmail.com>
2025-10-06 20:18:41 +0530

    Refactor UserInterface and schema: update bookMarkedPost to an array, add bio, profilePic, followers, and following fields

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -5,6 +5,10 @@ export interface UserInterface {
   email: string;
   passwordHash: string;
   role: string;
+  bio: string;
+  profilePic: string;
+  followers?: mongoose.Types.ObjectId;
+  following?: mongoose.Types.ObjectId;
   bookMarkedPost: mongoose.Types.ObjectId;
   createdAt: Date;
   updatedAt: Date;
@@ -29,17 +33,35 @@ const userSchema = new mongoose.Schema < UserInterface > (
       required: true,
     },
 
+    bio: {
+      type: String,
+      maxlength: 30
+    },
+
+    profilePic: {
+      type: String
+    },
+
+    followers: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }],
+
+    following: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User"
+    }],
+
     role: {
       type: String,
       default: "user",
       enum: ["user", "author"]
     },
 
-      bookMarkedPost: {
+      bookMarkedPost: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "Post",
-      default: []
-    }
+    }]
   },
   { timestamps: true }
 );
